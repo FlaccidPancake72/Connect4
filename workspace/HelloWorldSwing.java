2162fec969547de00425fc3c31ce8cd91462708f
Flaccid Pancake <127857667+FlaccidPancake72@users.noreply.github.com>
2025-04-03 14:13:46 +0000

    yesss

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -1,4 +1,29 @@
-import javax.swing.*;        
+import javax.swing.*;      
+
+
+/*A. grid layout --> 7 x 7 as squares, each square is a place u can put
+	I. Top row to drop things in then it shows where it goes at the bottom
+	II. Need isRed --> true if it is red, false then it's black 
+	III. Want 3 things: top is where drop, middle is our 2d array, bottom is reset and win counter 
+		i. COPY BOARD FROM CHESS
+B. Rules
+	I. Start as red
+	II. Turns: red --> black
+		i. need var to count what turn it is on 
+	III. win condition: 
+		i. four diagonal, vertical, horizontal
+		iv. isWin function: 
+			a. check this when you place it 
+			b. what it does: 
+				i. 3 if statements to check d, v, h
+					I. d: i+ , j+ and i-, j- and the opp direction --> turns false immediately if it hits another color 
+					II. and etc 
+	IV. Legal moves: 
+		i. literally verything BUT when column is filled
+			i. can have is filled Boolean, turns true when someone puts it at the spot where i = total row 
+				a. you know this bc when u click the drop at the top of the row ur i++
+C. */
+
 
 public class HelloWorldSwing {
     /**
