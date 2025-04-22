6262012119f82399044ec01b1618dbc63a7a41ab
Flaccid Pancake <127857667+FlaccidPancake72@users.noreply.github.com>
2025-04-22 14:15:30 +0000

    Co-authored-by: jLewis250 <jLewis250@users.noreply.github.com>

--- workspace/GameWindow.java
+++ workspace/GameWindow.java
@@ -1,11 +1,15 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*; 
+import java.net.*;
+import java.util.ArrayList;
 
 
 //Warning* Mess with this file at your own peril. You do not need to modify anything in here other than the 
@@ -13,48 +17,106 @@ import javax.swing.*;
 public class GameWindow {
     private JFrame gameWindow;    
     private Board board;
+    private final int WIDTH= 525;
+    
+    public static final int LISTENING_PORT = 9876;
+    private static ArrayList<ConnectionHandler> connections= new ArrayList();      // For communication with the connecting program.
     
     public static void main(String [] args){
         new GameWindow("blarg", "blorg");
+
+        int num = 0; 
+        ServerSocket listener;  // Listens for incoming connections.
+       
+        /* Accept and process connections forever, or until some error occurs. */
+        try {
+            listener = new ServerSocket(LISTENING_PORT);
+            System.out.println("Listening on port " + LISTENING_PORT);
+            while (true) {
+                Socket s = listener.accept();
+                ConnectionHandler handler = new ConnectionHandler(s, num);
+                num++; 
+                handler.start();
+                connections.add(handler);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Sorry, the server has shut down.");
+            System.out.println("Error:  " + e);
+            return;
+        }
+    }  // end main()
+    /**
+     *  Defines a thread that handles the connection with one
+     *  client.
+     */
+    private static class ConnectionHandler extends Thread {
+        Socket client;
+        private ObjectOutputStream os;
+        private ObjectInputStream is;
+        int num;
+        
+        ConnectionHandler(Socket socket, int num) {
+            client = socket;
+            this.num = num; 
+            try {
+            //set up your streams, make sure this order is reversed on the client side!
+                os = new ObjectOutputStream(socket.getOutputStream());
+                is = new ObjectInputStream(socket.getInputStream());
+                System.out.println("someone connected yapee");
+                } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                }
+        }
+        
+        //method to help us send information!
+        public void send(String input) {
+        try {
+                os.writeObject(input);
+                } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();                
+                }
+        }
+        
+        
+        public void run() {
+            while(client.isConnected()) {
+            try {
+            String input= (String)is.readObject();
+            //your code to send messages goes here.
+            if(input!= null) {
+            for(ConnectionHandler handler: connections) {
+            //make sure you don't try to access the handler from two different threads simultainously
+            synchronized(handler) {
+            handler.send(num + ": " + input);
+            }
+            }
+            }
+            }
+            catch (Exception e){
+                System.out.println("Error on connection " 
+                        + ": " + e);
+            }
+            }
+            connections.remove(client);
+            
+        }
     }
 
- 
     public GameWindow(String redName, String blackName) {
         gameWindow = new JFrame("THE SIGMA Connect 4");
         gameWindow.setLocation(100, 100);
         gameWindow.setLayout(new BorderLayout(20,20));
         this.board = new Board(this);
-        gameWindow.add(board, BorderLayout.CENTER);
-        gameWindow.add(buttons(), BorderLayout.SOUTH);
-        gameWindow.setMinimumSize(gameWindow.getPreferredSize());
-        gameWindow.setSize(gameWindow.getPreferredSize());
-        gameWindow.setResizable(false);
-        gameWindow.pack();
-        gameWindow.setVisible(true);
-        gameWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    }
-    
-    // Helper function to create data panel
+        JPanel boardPanel = new JPanel();
+        boardPanel.setLayout(null);
+        boardPanel.setPreferredSize(board.getPreferredSize());
+        boardPanel.add(board);
+        gameWindow.add(boardPanel, BorderLayout.CENTER);
 
-        private JPanel buttons() {
-        JPanel buttons = new JPanel();
-        buttons.setLayout(new GridLayout(1, 3, 10, 0));
-
-         final JButton d1 = new JButton("Drop here");
-
-         final JButton d2 = new JButton("Drop here");
-
-         final JButton d3 = new JButton("Drop here");
-
-         final JButton d4 = new JButton("Drop here");
-        
-         final JButton d5 = new JButton("Drop here");
-         
-         final JButton d6 = new JButton("Drop here");
-
-         final JButton d7 = new JButton("Drop here");
-        
-         final JButton quit = new JButton("Quit");
+        final JButton quit = new JButton("Quit");
         
         quit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -85,10 +147,56 @@ public class GameWindow {
                 }
             }
         }); 
-          
-        
-        buttons.add(nGame);
-        buttons.add(quit);
+
+        /* 
+         *         this.board = new Board(this);
+        JPanel boardPanel = new JPanel();
+        boardPanel.setLayout(null);
+        boardPanel.setPreferredSize(board.getPreferredSize());
+        boardPanel.add(board);
+        gameWindow.add(boardPanel, BorderLayout.CENTER);
+         */
+
+         JPanel bottomPanel = new JPanel(); 
+         bottomPanel.setSize(new Dimension(100,50)); 
+         bottomPanel.add(quit); 
+         bottomPanel.add(nGame); 
+    
+         gameWindow.add(bottomPanel, BorderLayout.SOUTH); 
+
+         JPanel topPanel = new JPanel(); 
+         topPanel.setSize(new Dimension(300,50)); 
+         JPanel buttons = buttons();
+       
+         buttons.setPreferredSize(new Dimension(WIDTH, 40));
+         topPanel.add(buttons);
+         topPanel.setVisible(true); 
+
+         gameWindow.add(topPanel, BorderLayout.NORTH); 
+
+        //gameWindow.add(buttons(), BorderLayout.NORTH);
+        gameWindow.setMinimumSize(gameWindow.getPreferredSize());
+        gameWindow.setResizable(false);
+        gameWindow.pack();
+        gameWindow.setVisible(true);
+        gameWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    
+
+    private int xLoc = 0; 
+    private boolean isRed = false; 
+    // Helper function to create data panel
+        private JPanel buttons() {
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridLayout(1, 3, WIDTH/7, 0));
+
+         final JButton d1 = new JButton("Drop here");
+         final JButton d2 = new JButton("Drop here");
+         final JButton d3 = new JButton("Drop here");
+         final JButton d4 = new JButton("Drop here");
+         final JButton d5 = new JButton("Drop here");
+         final JButton d6 = new JButton("Drop here");
+         final JButton d7 = new JButton("Drop here");
 
         buttons.add(d1); 
         buttons.add(d2); 
@@ -99,11 +207,63 @@ public class GameWindow {
         buttons.add(d7); 
         
         buttons.setPreferredSize(buttons.getMinimumSize());
+
+        d1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 1; 
+                isRed = !isRed; 
+			}
+		}); 
+
+        d2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 2; 
+                isRed = !isRed; 
+			}
+		}); 
+
+        d3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 3;
+                isRed = !isRed; 
+			}
+		}); 
+        
+        d4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 4; 
+                isRed = !isRed; 
+			}
+		}); 
+        
+        d5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 5; 
+                isRed = !isRed; 
+			}
+		}); 
+
+        d6.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 6; 
+                isRed = !isRed; 
+			}
+		}); 
+
+        d7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) { 
+                xLoc = 7; 
+                isRed = !isRed; 
+			}
+		}); 
         
         return buttons;
     }
-    
-    // public void checkmateOccurred (boolean whiteWins) {
-       
-    // }
 }
---
+++ workspace/SocketClient.java
@@ -0,0 +1,114 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+//client
+public class SocketClient {
+/*
+* Modify this example so that it opens a dialogue window using java swing, 
+* takes in a user message and sends it
+* to the server. The server should output the message back to all connected clients
+* (you should see your own message pop up in your client as well when you send it!).
+*  We will build on this project in the future to make a full fledged server based game,
+*  so make sure you can read your code later! Use good programming practices.
+*  ****HINT**** you may wish to have a thread be in charge of sending information 
+*  and another thread in charge of receiving information.
+*/
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
+        //get the localhost IP address, if server is running on some other IP, you need to use that
+        InetAddress host = InetAddress.getLocalHost();
+        final Socket socket = new Socket(host.getHostName(), 9876);
+		System.out.println("client connected");
+		final ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+		// write to socket using ObjectOutputStream
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+        //gui stuff
+        JFrame frame = new JFrame("server");
+        frame.setLayout(new GridLayout(3,1));
+        JTextField input = new JTextField(25);
+        frame.add(input);
+        JTextArea message = new JTextArea("", 100, 150);
+        message.setEditable(false);
+        frame.add(message);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+		Thread thread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						String input = (String) ois.readObject();
+						System.out.println("message received");
+						//System.out.println("received " + input);
+						if (input != null) {
+							//oos.writeObject(input);
+							message.setText(message.getText() + "\n" + input);
+							frame.repaint();
+						}
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+
+		thread.start();
+    	input.addActionListener(new ActionListener() {
+    		@Override
+    		public void actionPerformed(ActionEvent e) {
+                String text = input.getText();
+                try {
+					oos.writeObject(text);
+					System.out.println("sent "+text);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                input.setText(""); 
+            }
+    	});
+    	
+    	// closes the window when you exit.
+    	frame.addWindowListener((WindowListener) new WindowAdapter()
+        {
+           public void windowClosing(WindowEvent e)
+           {
+              if(socket.isConnected())
+				try {
+					oos.writeObject("exit");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+              try {
+                   Thread.sleep(1000); // Time for DisconnectMessage to actually be sent.
+               }
+               catch (InterruptedException e1) {
+               } 
+             //close resources
+             System.exit(0); 
+           }
+        });
+       // System.out.println((String)ois.readObject());
+        
+    }
+}
+
